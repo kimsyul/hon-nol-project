561eff1863f44b7fae1312f6bb8f9685c33e9543
Kim Seo-Yul <yul_the@naver.com>
2024-05-08 11:15:58 +0900

    no message

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import './assets/styles/reset.css'
+
+function App() : JSX.Element{
+
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App
