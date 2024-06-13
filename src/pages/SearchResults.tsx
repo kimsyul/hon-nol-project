f71b2eb4ff51762e7ed6ec2ec0655e32090c6dba
Kim Seo-Yul <yul_the@naver.com>
2024-06-13 22:48:13 +0900

    검색기능 다듬는 중

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import AdvancedSearchBar from '../components/common/AdvancedSearchBar';
-import useAdvancedSearchData from '../hook/useAdvancedSearchData';
+import PostList from '../pages/PostList';
 import queryString from 'query-string';
+import useAdvancedSearchData from '../hook/useAdvancedSearchData';
 import { FirestoreDocument } from '../hook/usePaginationData';
-import { ListContainer, ItemContainer, PostTitle, PostPreview, Info } from '../assets/styles/ListLayout';
-import { Container, Message } from '../pages/PostList';
-import { Link } from 'react-router-dom';
+// import { ListContainer, ItemContainer, PostTitle, PostPreview, Info } from '../assets/styles/ListLayout';
+// import { Link } from 'react-router-dom';
 
 const SearchResults: React.FC = () => {
     const location = useLocation();
@@ -24,16 +24,9 @@ const SearchResults: React.FC = () => {
     const [selectedSubregion, setSelectedSubregion] = useState((subregion as string) || '');
     const [selectedTheme, setSelectedTheme] = useState((theme as string) || '');
     const [selectedSubtheme, setSelectedSubtheme] = useState((subtheme as string) || '');
-    const [searchField, setSearchField] = useState(initialSearchField as keyof FirestoreDocument | 'all');
-
-    const { filteredData, loading, error } = useAdvancedSearchData({
-        searchTerm,
-        searchField,
-        selectedCategory1: selectedRegion,
-        selectedSubcategory1: selectedSubregion,
-        selectedCategory2: selectedTheme,
-        selectedSubcategory2: selectedSubtheme,
-    });
+    const [searchField, setSearchField] = useState<keyof FirestoreDocument | 'all'>(
+        (initialSearchField as keyof FirestoreDocument) || 'all',
+    );
 
     const handleSearch = (
         searchTerm: string,
@@ -52,43 +45,13 @@ const SearchResults: React.FC = () => {
     };
 
     useEffect(() => {
-        if (initialSearchTerm) {
-            setSearchTerm(initialSearchTerm as string);
-        }
-
-        if (initialSearchField) {
-            setSearchField(initialSearchField as keyof FirestoreDocument);
-        }
-
-        if (region) {
-            setSelectedRegion(region as string);
-        }
-
-        if (subregion) {
-            setSelectedSubregion(subregion as string);
-        }
-        if (theme) {
-            setSelectedTheme(theme as string);
-        }
-        if (subtheme) {
-            setSelectedSubtheme(subtheme as string);
-        }
-    }, [initialSearchField, initialSearchTerm, region, subregion, theme, subtheme]);
-
-    if (loading) {
-        return (
-            <Container>
-                <span className="loading loading-ring loading-lg"></span>
-            </Container>
-        );
-    }
-    if (error) {
-        return (
-            <Container>
-                <Message>오류가 발생했습니다. 다시 시도해 주세요.</Message>
-            </Container>
-        );
-    }
+        setSearchTerm((initialSearchTerm as string) || '');
+        setSelectedRegion((region as string) || '');
+        setSelectedSubregion((subregion as string) || '');
+        setSelectedTheme((theme as string) || '');
+        setSelectedSubtheme((subtheme as string) || '');
+        setSearchField((initialSearchField as keyof FirestoreDocument | 'all') || 'all');
+    }, [initialSearchTerm, initialSearchField, region, subregion, theme, subtheme]);
 
     return (
         <div>
@@ -101,25 +64,14 @@ const SearchResults: React.FC = () => {
                 initialSubtheme={selectedSubtheme}
                 initialSearchField={searchField}
             />
-            <ListContainer>
-                {filteredData.length === 0 ? (
-                    <Container>
-                        <Message>게시글이 없습니다.ㅠ_ㅠ</Message>
-                    </Container>
-                ) : (
-                    filteredData.map((post, index) => (
-                        <ItemContainer key={`${post.id}-${index}`}>
-                            <Link to={`/posts/${post.id}`}>
-                                <PostTitle>{post.title}</PostTitle>
-                                <PostPreview>{post.content.substring(0, 100)}...</PostPreview>
-                                <Info>
-                                    {post.region} - {post.subregion} / {post.theme} - {post.subtheme}
-                                </Info>
-                            </Link>
-                        </ItemContainer>
-                    ))
-                )}
-            </ListContainer>
+            <PostList
+                searchTerm={searchTerm}
+                selectedCategory1={selectedRegion}
+                selectedSubcategory1={selectedSubregion}
+                selectedCategory2={selectedTheme}
+                selectedSubcategory2={selectedSubtheme}
+                searchField={searchField}
+            />
         </div>
     );
 };
